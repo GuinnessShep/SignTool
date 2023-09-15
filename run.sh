633ecbc9bd8614d32297bcd6fb922fe1ce89477d
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-09-15 07:36:46 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-a
+apt update
+apt full-upgrade -y
+apt install unzip
+wget https://github.com/oobabooga/text-generation-webui/releases/download/installers/oobabooga_linux.zip
+unzip oobabooga_linux.zip
+rm -rf oobabooga_linux.zip
+cd oobabooga_linux
+rm -rf webui.py
+wget https://raw.githubusercontent.com/GuinnessShep/SignTool/master/webui.py
+bash start_linux.sh
