d2063acf8a110bd11b96888b0a25940249039a55
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-09-21 10:45:05 -0700

    Update runn3r.sh

--- runn3r.sh
+++ runn3r.sh
@@ -1,10 +1,9 @@
-curl https://sh.rustup.rs -sSf
-sh rustup-init.sh -s -- -y
-source /root/.cargo/env
+curl https://sh.rustup.rs -sSf | sh -s -- -y
+#source /root/.cargo/env
 source "$HOME/.cargo/env"
-/root/.cargo/bin/cargo install tunnelto
+#/root/.cargo/bin/cargo install tunnelto
 $HOME/.cargo/bin/cargo install tunnelto
-/root/.cargo/bin/tunnelto set-auth --key GeBoAW1CSmWK5SbfOXUmU8
+#/root/.cargo/bin/tunnelto set-auth --key GeBoAW1CSmWK5SbfOXUmU8
 $HOME/.cargo/bin/tunnelto set-auth --key GeBoAW1CSmWK5SbfOXUmU8
-/root/.cargo/bin/tunnelto --host localhost --subdomain sewagepuppy --port 7860
+#/root/.cargo/bin/tunnelto --host localhost --subdomain sewagepuppy --port 7860
 $HOME/.cargo/bin/tunnelto --host localhost --subdomain sewagepuppy --port 7860
